53477aa316dfd56d6de54280abdf43ca2d8f0727
Evan-Rousseau <61151018+Evan-Rousseau@users.noreply.github.com>
2021-01-04 14:33:10 +0100

    Login

---
+++ connexion-react/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Login from './login.component';
+import { BrowserRouter, BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Route 
+      path="/home"
+      render={props => (<App />)} />
+    <Route 
+      path="/login"
+      render={props => (<Login />)} />
+    
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ connexion-react/src/login.component.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+const LOGIN = 'login';
+const PASSWORD = 'password';
+export default class Stars extends React.PureComponent { 
+
+    state = { login: '', } 
+
+    static propTypes = { 
+        value: PropTypes.number.isRequired, 
+        style: PropTypes.any 
+    } 
+    // constructor(props) { 
+    //     super(props);
+    // } 
+    
+    render() {
+        return ( 
+        <div className="App">
+            <header className="App-header">
+                <form id="loginForm" onSubmit={this.login} style={this.props.style}>
+                    <span>Login: </span>
+                    <input type="text" id="login" name="login"></input>
+                    <br></br>
+                    <span>Password: </span>
+                    <input type="password" id="password" name="password"></input>
+                    <br></br>
+                    <input type="submit"></input>
+                </form>
+            </header>
+        </div>
+        );
+    }
+
+    login(event) {
+        let login = document.getElementById('login').value;
+        let password = document.getElementById('password').value;
+        if((login === LOGIN) && (password === PASSWORD)){
+            alert('Successfully connected');
+        }
+        else{
+            alert('Connexion failed');
+        }
+        event.preventDefault();
+    }
+}
